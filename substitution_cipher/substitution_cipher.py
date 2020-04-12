3474c376dde8f8437d1e2d1bef868774a1960ee2
Tommy Huynh <tommy.xhuynh@gmail.com>
2020-04-12 12:01:23 -0500

    implement bigram word model as a class

--- substitution_cipher/substitution_cipher.py
+++ substitution_cipher/substitution_cipher.py
@@ -6,10 +6,13 @@ import string
 TRAINING_CORPUS_PATH = "./data/train.txt"
 
 def main():
-    # build bigram letter model from training corpus
-    bigram_letter_model = get_bigram_letter_model(TRAINING_CORPUS_PATH)
+    # build bigram word model from training corpus
+    bigram_word_model = BigramWordModel(TRAINING_CORPUS_PATH)
+    print(bigram_word_model.get_word_probability("jesus"))
+    print(bigram_word_model.get_word_probability("moby"))
+    print(bigram_word_model.get_word_probability("winste@d"))
 
-    # build bigram word model
+    # build bigram sentence model
 
     # encrypt the message
     text = "holla"
@@ -23,57 +26,103 @@ def main():
     decrypted_text = decrypt(cipher, encrypted_text)
     print(decrypted_text)
 
-def get_bigram_letter_model(corpus_path):
-    """Builds the bigram letter model from the given corpus
-
-    Parameters
-    ----------
-    corpus_path
-        The path to the training corpus
-
-    Returns
-    -------
-    dict
-        The bigram probablity model where dict["ab"] = p(a|b)
-
+class BigramWordModel:
     """
-    # initialize unigram letter count (used in denominator for bigram probability)
-    unigram_count = {}
-    for letter in range(ord('a'), ord('z')+1):
-        unigram_count[chr(letter)] = 0
+    A representation of the Bigram Word Model
+    ...
+    Attributes
+    ----------
+    __prefix_unigram_model
+        The dict mapping a character to its probability as a leading character in words
+    __bigram_model
+        The dict mapping a bigram to its probability, where dict["ab"] = p(a|b)
 
-    # initialize bigram letter sequence count 
-    bigram_seq_count = {}
-    for letter1 in range(ord('a'), ord('z')+1):
-        for letter2 in range(ord('a'), ord('z')+1):
-            seq = chr(letter1) + chr(letter2)
-            bigram_seq_count[seq] = 0
+    Methods
+    -------
+    train(corpus_path)
+        Train the model on the corpus given by corpus_path
+    """
+    __prefix_unigram_model = {}
+    __bigram_model = {}
 
-    # count bigram sequences from training corpus
-    with open(corpus_path, "r") as fin:
-        for line in fin.readlines():
-            # strip punctuation
-            str = line.translate(line.maketrans('', '', string.punctuation))
-            # strip digits
-            str = str.translate(str.maketrans('', '', string.octdigits))
-            str = str.lower()
-            for word in str.split():
-                for i in range(0, len(word)-1):
-                    if not (is_ascii_alpha(word[i]) and is_ascii_alpha(word[i+1])):
-                        # only account for alpha characters in bigram seq count
-                        continue
-                    unigram_count[word[i]] += 1
-                    bigram_seq_count[word[i:i+2]] += 1
+    def __init__(self, corpus_path):
+        """
+        Parameters
+        ----------
+        corpus_path
+            The path to the training corpus
+        """
+        # initialize unigram letter count (used in denominator for bigram probability)
+        # and prefix unigram count (used in calculating prefix unigram probability)
+        unigram_count = {}
+        prefix_unigram_count = {}
+        for letter in range(ord('a'), ord('z')+1):
+            unigram_count[chr(letter)] = 0
+            prefix_unigram_count[chr(letter)] = 0
 
-    # calculate bigram probability model
-    bigram_probability_model = {}
-    for bigram in bigram_seq_count:
-        # applying +1 smoothing
-        numerator = float(bigram_seq_count[bigram] + 1)
-        denominator = float(unigram_count[bigram[0]] + 26)
-        bigram_probability_model[bigram] = numerator / denominator
+        # initialize bigram letter sequence count 
+        bigram_seq_count = {}
+        for letter1 in range(ord('a'), ord('z')+1):
+            for letter2 in range(ord('a'), ord('z')+1):
+                seq = chr(letter1) + chr(letter2)
+                bigram_seq_count[seq] = 0
 
-    return bigram_probability_model
+        word_count = 0 # used to calculate prefix unigram probability
+
+        # count bigram sequences from training corpus
+        with open(corpus_path, "r") as fin:
+            for line in fin.readlines():
+                # strip punctuation
+                str = line.translate(line.maketrans('', '', string.punctuation))
+                # strip digits
+                str = str.translate(str.maketrans('', '', string.octdigits))
+                str = str.lower()
+                for word in str.split():
+                    if is_ascii_alpha(word[0]):
+                        # update prefix unigram model
+                        prefix_unigram_count[word[0]] += 1
+                        word_count += 1
+                    # update bigram model
+                    for i in range(0, len(word)-1):
+                        if not (is_ascii_alpha(word[i]) and is_ascii_alpha(word[i+1])):
+                            # only account for alpha characters in bigram seq count
+                            continue
+                        unigram_count[word[i]] += 1
+                        bigram_seq_count[word[i:i+2]] += 1
+
+        # calculate prefix unigram probability model
+        for letter in range(ord('a'), ord('z')):
+            # apply +1 smoothing
+            self.__prefix_unigram_model[chr(letter)] = float(prefix_unigram_count[chr(letter)]+1) / float(word_count+26)
+
+        # calculate bigram probability model
+        for bigram in bigram_seq_count:
+            # apply +1 smoothing
+            numerator = float(bigram_seq_count[bigram] + 1)
+            denominator = float(unigram_count[bigram[0]] + 26)
+            self.__bigram_model[bigram] = numerator / denominator
+
+    def get_word_probability(self, word):
+        """Calculates the probability of a word using the given bigram word model
+
+        Parameters
+        ----------
+        word
+            The word to calculate probability for (in lowercase)
+
+        Returns
+        -------
+        float
+            The probability of the word
+        """
+        word = strip_non_ascii_alpha(word)
+        if len(word) == 0:
+            return 1 # don't penalize non-ascii-alpha string
+
+        probability = self.__prefix_unigram_model[word[0]]
+        for i in range(0, len(word)-1):
+            probability *= self.__bigram_model[word[i:i+2]]
+        return probability
 
 def is_ascii_alpha(str):
     """Checks if all the characters in the given str are ASCII letters
@@ -89,6 +138,25 @@ def is_ascii_alpha(str):
         True if all the characters are ASCII characters; False otherwise
     """
     return all(ord(c) >= ord('a') and ord(c) <= ord('z') for c in str)
+
+def strip_non_ascii_alpha(str):
+    """Removes non-ASCII-alpha characters from the given string
+
+    Parameters
+    ----------
+    str
+        The string to remove non-ASCII-alpha characters from
+
+    Returns
+    -------
+    str
+        The string with just ASCII alpha characters
+    """
+    ascii_alphas = []
+    for letter in str:
+        if is_ascii_alpha(letter):
+            ascii_alphas.append(letter)
+    return "".join(ascii_alphas)
 
 def create_rotation_cipher(offset):
     """Creates a substitution cipher based on a rotation offset
